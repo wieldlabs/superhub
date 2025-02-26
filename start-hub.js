d99beb52432171907a6a39cbf44aaf52a8053861
jcdenton.eth <jc@beb.xyz>
2025-02-26 15:08:53 -0800

    feat: increment version to 0.0.4

--- start-hub.js
+++ start-hub.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const dotenv = require("dotenv");
 
 const { router: apiKeyRouter } = require("./express-routes/apikey");
 const { router: farcasterRouter } = require("./express-routes/farcaster");
 
 const { connectDB } = require("./connectdb");
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Check required environment variables
+const REQUIRED_ENV_VARS = [
+  "JWT_SECRET",
+  "MONGO_URL",
+  "MEMCACHED_URL",
+  "NODE_ENV",
+];
+
+const missingVars = REQUIRED_ENV_VARS.filter((envVar) => {
+  if (!process.env[envVar]) {
+    console.error(`${envVar} is not set. Please set it (e.g. .env file)!`);
+    return true;
+  }
+  return false;
+});
+
+if (missingVars.length > 0) {
+  console.error("Exiting...");
+  process.exit(1);
+}
 
 const app = express();
 
